d30fcdbeed9589beb67eeb2b4bb6811dcf1d5368
odrum428 <oxkeita@gmail.com>
2019-11-27 14:53:39 +0900

    設定変数を秘匿しつつ参照できるように修正

---
+++ cw_scraping/settings.py
@@ -0,0 +1,10 @@
+# coding: UTF-8
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+LOGIN_EMAIL = os.environ.get("LOGIN_EMAIL")
+LOGIN_PASS = os.environ.get("LOGIN_PASS")
