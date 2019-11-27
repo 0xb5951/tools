2e460bc02f20453281625f949ff8a5971c6aee93
odrum428 <oxkeita@gmail.com>
2019-11-27 17:52:03 +0900

    依頼の情報が正しく取得できるところまで実装

--- cw_scraping/scrap.py
+++ cw_scraping/scrap.py
@@ -2,11 +2,18 @@ import chromedriver_binary
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+import datetime
+
 import settings
 
 # 設定値
 LOGIN_URL = 'https://crowdworks.jp/login'
 SCRAP_URL = 'https://crowdworks.jp/public/jobs/recommendations/recommend_for_you'
+
+REC_CATE = 'システム開発'
+REC_LEVEL = '普通'
+REC_LEN = '普通'
+REC_TYPE = '継続'
 
 class ScrapeWithLogin():
     def __init__(self, url):
@@ -44,11 +51,31 @@ class ScrapeWithLogin():
         # 依頼の全体要素をリストに格納
         recommend_works = soup.select('li[data-job_offer_id]')
 
+        work_infos = []
+        work_infos += 'id,title,category,price,post_num,deadline\n'
+
         for work in recommend_works:
             work_id = work['data-job_offer_id']
-            print(work_id)
+            work_a_links = work.find_all('a')
+            work_bolds = work.find_all('b')
+
+            work_title = work_a_links[0].text.strip()
+            work_category = work_a_links[1].text.strip()
+
+            work_price = work_bolds[0].text.strip()
+            work_post_num = work_bolds[1].text.strip()
+            work_deadline = work_bolds[2].text.strip()
             
-        return
+            # 情報を書き込み
+            work_infos += '{0},{1},{2},{3},{4},{5}\n'.format(work_id, work_title, work_category, work_price, work_post_num, work_deadline)
+
+        dt_now = datetime.datetime.now().strftime('%Y-%m-%d')
+        csv_name = './scrap_data/{0}_{1}_{2}_{3}_{4}.csv'.format(REC_CATE, REC_LEVEL, REC_LEN, REC_TYPE, dt_now)
+        with open(csv_name, 'w', encoding='utf-8') as f:
+            f.write(''.join(work_infos))
+        
+        print('process complete!')
+        return 0
 
 if __name__ == '__main__':
     scrap = ScrapeWithLogin(LOGIN_URL)
