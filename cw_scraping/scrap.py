0f650bdaea94876cfc054cf3fa6f5b4e29ab6304
odrum428 <oxkeita@gmail.com>
2019-11-27 14:59:22 +0900

    isortを実行

--- cw_scraping/scrap.py
+++ cw_scraping/scrap.py
@@ -1,3 +1,15 @@
+import chromedriver_binary
+from selenium import webdriver
+
 import settings
 
-print(settings.LOGIN_EMAIL)
+
+class ScrapeLoginAuthSite():
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.url = "WEBSITE_URL"
+        #chrome driver -headless mode
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=options)
