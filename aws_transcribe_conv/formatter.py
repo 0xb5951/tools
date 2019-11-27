58032d34a339ff36a1cec283daa2cd564f18f5bf
odrum428 <oxkeita@gmail.com>
2019-11-28 01:09:27 +0900

    話者データの並び順を確認

--- aws_transcribe_conv/formatter.py
+++ aws_transcribe_conv/formatter.py
@@ -21,17 +21,40 @@ def extract_process(target_files):
         data = json.load(f)
 
         # 話者の情報が入っている部分
-        speaker_list = data['results']['speaker_labels']
+        speaker_list = data['results']['speaker_labels']['segments']
+        count = 0
+        # print(len(speaker_list))
+        # length = len(speaker_list)
+        for i in range(len(speaker_list)-1):
+            # print('start_time:' + speaker_list[i]['start_time'] + ' endtime:' + speaker_list[i]['end_time'])
+            tmp = float(speaker_list[i]['start_time'])
+            if tmp > float(speaker_list[i + 1]['start_time']):
+                count += 1
+                print(type(tmp))
+                print('tmp:' + tmp)
+                print('next:' + speaker_list[i + 1]['start_time'])
+        
+        print(count)
 
-        # listに格納する
-        transcribe_list = data['results']['items']
+        # print(type(speaker_list))
+        # print(speaker_list[0])
+        # # listに格納する
+        # transcribe_list = data['results']['items']
+        # # start_timeの順番にソート
+        # transcribe_list.sort(key=lambda x: x['start_time'])
+        # # 連続した話者情報をまとめる
+
+        # print(transcribe_list[0])
+        # print(transcribe_list[0].update(speaker_list[0]))
+        # print(transcribe_list[0])
         # 話者情報と結合
+        # for index,item in enumerate(transcribe_list):
+        #     item.update(speaker_list[index])
+        # print(transcribe_list)
+        # # 信頼性が低い要素を削除
+        # transcribe_list = [item for item in transcribe_list if float(item['alternatives'][0]['confidence']) > 0.5]
 
-        # 信頼性が低い要素を削除
-        transcribe_list = [item for item in transcribe_list if float(item['alternatives'][0]['confidence']) > 0.5]
-        # start_timeの順番にソート
-        transcribe_list.sort(key=lambda x: x['start_time'])
-        print(transcribe_list)
+        # print(transcribe_list)
     return 
 
 
