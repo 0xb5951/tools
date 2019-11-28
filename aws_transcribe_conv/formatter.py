f0d77946bda3239aefc5241c3db2d236dc9fab9f
odrum428 <oxkeita@gmail.com>
2019-11-28 13:25:00 +0900

    話者のタイムラインに変換するところまで実装

--- aws_transcribe_conv/formatter.py
+++ aws_transcribe_conv/formatter.py
@@ -24,7 +24,7 @@ def extract_process(target_files):
         speaker_list = data['results']['speaker_labels']['segments']
         # 文字起こし部分
         transcribe_list = data['results']['items']
-        print(transcribe_list[0]['start_time'])
+        # print(transcribe_list[0]['start_time'])
         # transcribe_list.sort(key=lambda x: x['start_time'])
 
         # for segment in transcribe_list:
@@ -36,15 +36,11 @@ def extract_process(target_files):
         # 話者ごとにまとめた文字起こしデータ
         trans_segments = []
         tran_index = 0
-        print(transcribe_list[0])
+
+        # 話者データと文字起こしをくっつける
         for index, segment in enumerate(speaker_list):
-            # start_item
             start_segment = segment['items'][0]
             end_segment = segment['items'][-1]
-            # print(segment['start_time'])
-            # print(start_segment)
-            # print(segment['end_time'])
-            # print(end_segment)
 
             tmp_list = []
             while 1:
@@ -57,46 +53,19 @@ def extract_process(target_files):
                 except KeyError:
                     ''
                 tran_index += 1
-                
-
-
-
             # 最後にまとめて格納
             trans_segments += [{'speaker_label': segment['speaker_label'],'items': tmp_list}]
 
-        print(trans_segments[0:5])
+        output_txt = ''
+        # タイムライン形式に変換
+        for trans_segment in trans_segments:
+            output_txt += trans_segment['speaker_label'] + ' :'
+            print(trans_segment)
+            for item in trans_segment['items']:
+                output_txt += item['content']
+            output_txt += '\n'
 
-            # tmp = float(speaker_list[i]['start_time'])
-            # if tmp > float(speaker_list[i + 1]['start_time']):
-            #     count += 1
-            #     print(type(tmp))
-            #     print('tmp:' + tmp)
-            #     print('next:' + speaker_list[i + 1]['start_time'])
-
-        # # 開始のセグメント
-        # data['results']['speaker_labels']['segments'][i][0]
-        # # 終わりのセグメント
-        # data['results']['speaker_labels']['segments'][i][-1]
-
-        
-        # print(count)
-
-        # print(type(speaker_list))
-        # print(speaker_list[0])
-
-        # # 連続した話者情報をまとめる
-
-        # print(transcribe_list[0])
-        # print(transcribe_list[0].update(speaker_list[0]))
-        # print(transcribe_list[0])
-        # 話者情報と結合
-        # for index,item in enumerate(transcribe_list):
-        #     item.update(speaker_list[index])
-        # print(transcribe_list)
-        # # 信頼性が低い要素を削除
-        # transcribe_list = [item for item in transcribe_list if float(item['alternatives'][0]['confidence']) > 0.5]
-
-        # print(transcribe_list)
+        print(output_txt)
     return 
 
 
