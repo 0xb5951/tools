a3dd7d860319f192c315d4edd0e86082db34f087
odrum428 <oxkeita@gmail.com>
2019-11-28 12:33:31 +0900

    文字起こしと話者データを繋げる部分を実装

--- aws_transcribe_conv/formatter.py
+++ aws_transcribe_conv/formatter.py
@@ -22,26 +22,68 @@ def extract_process(target_files):
 
         # 話者の情報が入っている部分
         speaker_list = data['results']['speaker_labels']['segments']
-        count = 0
-        # print(len(speaker_list))
-        # length = len(speaker_list)
-        for i in range(len(speaker_list)-1):
-            # print('start_time:' + speaker_list[i]['start_time'] + ' endtime:' + speaker_list[i]['end_time'])
-            tmp = float(speaker_list[i]['start_time'])
-            if tmp > float(speaker_list[i + 1]['start_time']):
-                count += 1
-                print(type(tmp))
-                print('tmp:' + tmp)
-                print('next:' + speaker_list[i + 1]['start_time'])
+        # 文字起こし部分
+        transcribe_list = data['results']['items']
+        print(transcribe_list[0]['start_time'])
+        # transcribe_list.sort(key=lambda x: x['start_time'])
+
+        # for segment in transcribe_list:
+        #     if 'start_time' in segment:
+        #         ''
+        #     else: 
+        #         print(segment)
+
+        # 話者ごとにまとめた文字起こしデータ
+        trans_segments = []
+        tran_index = 0
+        print(transcribe_list[0])
+        for index, segment in enumerate(speaker_list):
+            # start_item
+            start_segment = segment['items'][0]
+            end_segment = segment['items'][-1]
+            # print(segment['start_time'])
+            # print(start_segment)
+            # print(segment['end_time'])
+            # print(end_segment)
+
+            tmp_list = []
+            while 1:
+                tmp_list += transcribe_list[tran_index]['alternatives']
+                try:
+                    if end_segment['start_time'] == transcribe_list[tran_index]['start_time'] and \
+                        end_segment['end_time'] == transcribe_list[tran_index]['end_time']:
+                        tran_index += 1
+                        break
+                except KeyError:
+                    ''
+                tran_index += 1
+                
+
+
+
+            # 最後にまとめて格納
+            trans_segments += [{'speaker_label': segment['speaker_label'],'items': tmp_list}]
+
+        print(trans_segments[0:5])
+
+            # tmp = float(speaker_list[i]['start_time'])
+            # if tmp > float(speaker_list[i + 1]['start_time']):
+            #     count += 1
+            #     print(type(tmp))
+            #     print('tmp:' + tmp)
+            #     print('next:' + speaker_list[i + 1]['start_time'])
+
+        # # 開始のセグメント
+        # data['results']['speaker_labels']['segments'][i][0]
+        # # 終わりのセグメント
+        # data['results']['speaker_labels']['segments'][i][-1]
+
         
-        print(count)
+        # print(count)
 
         # print(type(speaker_list))
         # print(speaker_list[0])
-        # # listに格納する
-        # transcribe_list = data['results']['items']
-        # # start_timeの順番にソート
-        # transcribe_list.sort(key=lambda x: x['start_time'])
+
         # # 連続した話者情報をまとめる
 
         # print(transcribe_list[0])
