7dcdf9a468b9318401ffe03ad2d69de1e445c0e3
odrum428 <oxkeita@gmail.com>
2019-11-28 13:55:10 +0900

    一旦完成

--- aws_transcribe_conv/formatter.py
+++ aws_transcribe_conv/formatter.py
@@ -1,71 +1,51 @@
 import os
 import json
 
-def conv_color(confidence: float) -> str:
-    if confidence == 1:
-        return "black"
-    elif confidence >= 0.9:
-        return "gray"
-    else:
-        return "silver"
-
-    # for item in :
-    #     confidence = float(item['alternatives'][0]['confidence'])
-
-    #     print(f'<font color="{color}">', end='')
-    #     print(item['alternatives'][0]['content'], end='')
-    #     print('</font>', end='')
-
 def extract_process(target_files):
-    with open('./transcript_data/' +target_files[0]) as f:
-        data = json.load(f)
+    for target_file in target_files:
+        with open('./transcript_data/' +target_file) as f:
+            data = json.load(f)
 
-        # 話者の情報が入っている部分
-        speaker_list = data['results']['speaker_labels']['segments']
-        # 文字起こし部分
-        transcribe_list = data['results']['items']
-        # print(transcribe_list[0]['start_time'])
-        # transcribe_list.sort(key=lambda x: x['start_time'])
+            # 話者の情報が入っている部分
+            speaker_list = data['results']['speaker_labels']['segments']
+            # 文字起こし部分
+            transcribe_list = data['results']['items']
 
-        # for segment in transcribe_list:
-        #     if 'start_time' in segment:
-        #         ''
-        #     else: 
-        #         print(segment)
+            # 話者ごとにまとめた文字起こしデータ
+            trans_segments = []
+            tran_index = 0
 
-        # 話者ごとにまとめた文字起こしデータ
-        trans_segments = []
-        tran_index = 0
+            # 話者データと文字起こしをくっつける
+            for index, segment in enumerate(speaker_list):
+                start_segment = segment['items'][0]
+                end_segment = segment['items'][-1]
 
-        # 話者データと文字起こしをくっつける
-        for index, segment in enumerate(speaker_list):
-            start_segment = segment['items'][0]
-            end_segment = segment['items'][-1]
+                tmp_list = []
+                while 1:
+                    tmp_list += transcribe_list[tran_index]['alternatives']
+                    try:
+                        if end_segment['start_time'] == transcribe_list[tran_index]['start_time'] and \
+                            end_segment['end_time'] == transcribe_list[tran_index]['end_time']:
+                            tran_index += 1
+                            break
+                    except KeyError:
+                        ''
+                    tran_index += 1
+                # 最後にまとめて格納
+                trans_segments += [{'speaker_label': segment['speaker_label'],'items': tmp_list}]
 
-            tmp_list = []
-            while 1:
-                tmp_list += transcribe_list[tran_index]['alternatives']
-                try:
-                    if end_segment['start_time'] == transcribe_list[tran_index]['start_time'] and \
-                        end_segment['end_time'] == transcribe_list[tran_index]['end_time']:
-                        tran_index += 1
-                        break
-                except KeyError:
-                    ''
-                tran_index += 1
-            # 最後にまとめて格納
-            trans_segments += [{'speaker_label': segment['speaker_label'],'items': tmp_list}]
+            output_txt = ''
+            # タイムライン形式に変換
+            for trans_segment in trans_segments:
+                output_txt += trans_segment['speaker_label'] + ' :'
+                for item in trans_segment['items']:
+                    output_txt += item['content']
+                output_txt += '\n'
 
-        output_txt = ''
-        # タイムライン形式に変換
-        for trans_segment in trans_segments:
-            output_txt += trans_segment['speaker_label'] + ' :'
-            print(trans_segment)
-            for item in trans_segment['items']:
-                output_txt += item['content']
-            output_txt += '\n'
-
-        print(output_txt)
+            # ファイルに出力
+            file_name = 'timeline_data/' +target_file.split('.')[0] + '.txt'
+            with open(file_name, 'w', encoding='utf-8') as f:
+                f.write(output_txt)
     return 
 
 
