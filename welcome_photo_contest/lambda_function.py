b36d68ddd9c514c06004ee40e13610aa56b62419
odrum428 <oxkeita@gmail.com>
2020-02-12 15:58:51 +0900

    ファイルが取得できることを確認

--- welcome_photo_contest/lambda_function.py
+++ welcome_photo_contest/lambda_function.py
@@ -1,8 +1,7 @@
 import json
 import boto3
 import os
-
-
+import requests
 
 #画像ダウンロード
 def get_image(image_url,token):
@@ -14,10 +13,9 @@ def get_image(image_url,token):
     return image.content
 
 def get_slack_file(token,file_id):
-    bot_token = os.getenv('SLACK_BOT_USER_ACCESS_TOKEN')
-    headers = {'Authorization': 'Bearer {}'.format(bot_token)}
+    bot_token = os.environ["SLACK_BOT_USER_ACCESS_TOKEN"]
 
-    response = requests.get('https://slack.com/api/files.info'+'?file='+file_id,headers=headers)
+    response = requests.get('https://slack.com/api/files.info?token='+bot_token+'&file='+file_id)
 
     return json.loads(response.text)
 
@@ -56,8 +54,10 @@ def lambda_handler(event, context):
     # TODO implement
     print(event)
 
+    print(event['event']['files'][0]['id'])
+    file_id = event['event']['files'][0]['id']
     # 画像取得？
-    file_res = get_slack_file(bot_token, event['event']['file_id'])
+    file_res = get_slack_file(bot_token, file_id)
     print(file_res)
     if event['event']['text'] in '<@UTHADKVA6>':
         print('check run')
